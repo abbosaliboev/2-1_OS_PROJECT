e343f43af09d77dd8fba83f84729d8df82a41686
Quivy <43814194+QuivyGM@users.noreply.github.com>
2024-05-29 21:07:38 +0900

    Update main.py

--- MAIN_APP/main.py
+++ MAIN_APP/main.py
@@ -24,7 +24,7 @@ class MainScreen(Screen):
 #save name to "name"
 
 #find referring product in database and save info to result
-cur.execute("SELECT * FROM product WHERE name=?", (result[0],))
+cur.execute("SELECT * FROM product WHERE name=Cocacola", (result[0],))
 result = cur.fetchone()
 
 class SecondScreen(Screen):
