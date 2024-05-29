0d534d27680c1eef46440b981e73ea45a5d8071e
Quivy <43814194+QuivyGM@users.noreply.github.com>
2024-05-29 10:13:48 +0900

    add interface and db
    attempt to connect interface code and db code

--- MAIN_APP/main.py
+++ MAIN_APP/main.py
@@ -1,21 +1,46 @@
-#import camera code
+# main.py
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+import time
 
-#send picture to yolo
+import sqlite3
+con=sqlite3.connect("product.db")
+cur con.cursor()
 
-#extract product name
 
-#find name from database
-cur.execute("SELECT * FROM product WHERE name=?", (result[0],))
-result = cur.fetchone()
-print("\nfrom product:")
-print(result[0])    #name
-print(result[1])    #brand company
-print(result[2])    #price
-print(result[3])    #capacity
-print(result[4])    #calorie
+class MainScreen(Screen):
+    def capture_image(self):
+        camera = self.ids['camera']
+        camera.export_to_png("photos/IMG.png")
+        print("Captured")
 
-#con.close()
+#Yolo access IMG/png from photo file
+#save name to "name"
 
-#show on screen
+#go to my.kv for text output
 
-#other features
+class SecondScreen(Screen):
+    def toggle_microphone(self):
+        # Add logic for enabling/disabling the microphone here
+        pass
+
+class SettingScreen(Screen):
+    pass
+
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(SecondScreen(name='second'))
+        sm.add_widget(SettingScreen(name='setting'))
+        return sm
+
+
+if __name__ == '__main__':
+    MyApp().run()
