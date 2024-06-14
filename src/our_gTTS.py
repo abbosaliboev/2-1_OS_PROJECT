5dd2717b1dfd4a6fcccf0ae5da6a26a9038f2a46
Quivy <43814194+QuivyGM@users.noreply.github.com>
2024-06-14 22:08:11 +0900

    tts case for data and product

--- src/our_gTTS.py
+++ src/our_gTTS.py
@@ -34,6 +34,36 @@ def announce_product_info(product_name):
     pygame.mixer.quit() # Win32 error를 해결하기 위해 pygame.mixer 종료해주기
                         # 종료해주지 않고 다시 실행하면 pygame.mixer가 이용하는 mp3파일이 삭제됨(사용중인 파일을 삭제하는 오류)
 
+def announce_product_data(product_name):
+    product_info = get_product_info(product_name)
+    if product_info is not None:
+        id, name, korean_name, brand, price, capacity, calories = product_info
+        # 제품 정보를 한국어로 출력
+        tts_text = f"해당 제품은 {korean_name} {capacity}입니다. 제조사는 {brand}, 편의점 가격은 {price}원, 용량은 {capacity}이며 {calories} 칼로리를 가지고 있습니다."
+    else:
+        tts_text = "죄송합니다. 현재 해당 제품은 등록되어 있지 않습니다."
+
+    # gTTS를 사용하여 텍스트를 음성 파일로 저장
+    tts = gTTS(text=tts_text, lang='ko', slow=False)
+    
+    if os.path.exists("product_info.mp3"):
+        os.remove("product_info.mp3")
+    tts.save("product_info.mp3")
+
+    # pygame을 사용하여 음성 파일 재생
+    pygame.mixer.init()
+    pygame.mixer.music.load("product_info.mp3")
+    pygame.mixer.music.play()
+
+    # 음성 출력이 끝날 때까지 대기
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    pygame.mixer.quit() # Win32 error를 해결하기 위해 pygame.mixer 종료해주기
+                        # 종료해주지 않고 다시 실행하면 pygame.mixer가 이용하는 mp3파일이 삭제됨(사용중인 파일을 삭제하는 오류)
+
+
+
 def announce_basket_info(item_counts, total_price):
     # 장바구니의 제품 이름을 사용하여 제품 정보를 로드하고, 이를 한국어로 출력
     product_names_str = ', '.join([f"{name} {info['count']}개" for name, info in item_counts.items()])
@@ -54,9 +84,15 @@ def announce_basket_info(item_counts, total_price):
 
     pygame.mixer.quit()
     
-def main(product_name):
+def main(product_name, case):
     # 해당 제품의 정보를 음성으로 출력
-    announce_product_info(product_name)
+    if case == 1:
+        announce_product_info(product_name)
+    elif case == 2:
+        announce_product_data(product_name)
+    else:
+        def announce_basket_info(item_counts, total_price)
+
 
 if __name__ == "__main__":
     main()
